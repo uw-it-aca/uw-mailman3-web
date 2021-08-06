4a00db659735db76331e3df2149a933d8c968bc3
Stephen Mats Mats <willey@uw.edu>
2021-08-06 15:02:59 +0000

    maybe wait for mailman before adding fixture

--- docker/app_start.sh
+++ docker/app_start.sh
@@ -4,6 +4,10 @@ if [ "$ENV"  = "localdev" ]
 then
 
   python manage.py migrate
+  until curl --silent --connect-timeout 10 http://uw-mailman3-core:8001/3.1/lists > /dev/null; do
+                >&2 echo "waiting for mailman"
+                sleep 5
+        done
   python manage.py loaddata superuser.json
 
 fi
