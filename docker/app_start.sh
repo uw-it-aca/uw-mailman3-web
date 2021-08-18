aab46784437c1882a410c1954fbd961f7a1e2ee7
Craig M. Stimmel <cstimmel@uw.edu>
2021-08-18 14:10:10 -0700

    Install postorius from submodule on localdev

--- docker/app_start.sh
+++ docker/app_start.sh
@@ -3,6 +3,8 @@
 if [ "$ENV"  = "localdev" ]
 then
 
+  pip install -e ./postorius
+
   python manage.py migrate
   until curl --silent --connect-timeout 10 http://uw-mailman3-core:8001/3.1/lists > /dev/null; do
                 >&2 echo "waiting for mailman"
