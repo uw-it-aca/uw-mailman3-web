44ff37b93d4ed6aa1aa34faaa7a8e52667682836
Craig M. Stimmel <cstimmel@uw.edu>
2021-10-12 11:26:59 -0700

    Works with uw-mailman3-core cicd branch

--- docker/app_start.sh
+++ docker/app_start.sh
@@ -8,7 +8,7 @@ then
   pip install -e ./hyperkitty
 
   python manage.py migrate
-  until curl --silent --connect-timeout 10 http://uw-mailman3-core:8001/3.1/lists > /dev/null; do
+  until curl --silent --connect-timeout 10 http://uw-mailman3-core:8000/3.1/lists > /dev/null; do
                 >&2 echo "waiting for mailman"
                 sleep 5
         done
