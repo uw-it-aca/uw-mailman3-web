78b42bb3da34a2ba16af274386e70ce50542cafe
Jim Laney <jlaney@uw.edu>
2022-12-19 11:09:26 -0800

    add url for robots.txt

--- docker/urls.py
+++ docker/urls.py
@@ -2,12 +2,13 @@ import os
 from .base_urls import *
 from django.contrib import admin
 from django.urls import include, re_path, reverse_lazy
-from django.views.generic.base import RedirectView
+from django.views.generic.base import RedirectView, TemplateView
 
 urlpatterns += [
     re_path(r'^$', RedirectView.as_view(
-        url=reverse_lazy('list_index'),
-        permanent=True)),
+        url=reverse_lazy('list_index'), permanent=True)),
+    re_path(r'^robots\.txt$', TemplateView.as_view(
+        template_name='robots.txt', content_type='text/plain')),
     re_path(r'^postorius/', include('postorius.urls')),
     re_path(r'^hyperkitty/', include('hyperkitty.urls')),
     re_path(r'', include('django_mailman3.urls')),
