b28805db181e2c823fb228a63efbe11c7ff6c3d7
Stephen S Mats Mats <willey@washington.edu>
2023-10-23 15:33:37 -0700

    required addition of allauth middleware

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setup(
     include_package_data=True,
     install_requires=[
         'Django~=3.2',
-        'django-allauth',
+        'django-allauth>=0.56',
         'django-compressor==2.4.1',
         'django-mailman3==1.3.11',
         'djangorestframework',
