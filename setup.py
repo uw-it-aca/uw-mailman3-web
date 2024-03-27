65897313fd6c7d39c07e7326c377529af2c0c525
mike seibel <mikes@washington.edu>
2024-03-27 14:45:29 -0700

    no comma

--- setup.py
+++ setup.py
@@ -27,7 +27,6 @@ setup(
         'lxml==4.9.4',
         'whoosh',
         'UW-Django-SAML2~=1.6',
-        'lxml==4.9.4'
         'uw_list_manager @ git+https://github.com/uw-it-aca/uw-list-manager@develop#egg=uw-list-manager',
     ],
     license='Apache License, Version 2.0',  # example license
